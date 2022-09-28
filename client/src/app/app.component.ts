8b81ac6bb3248081165bce3091a8496c852d7f9b
veasna <veasnakoeun@gmail.com>
2022-09-29 00:14:28 +0700

    seconday

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'client';
+  users:any;
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getuser();
+  }
+  getuser(){
+    this.http.get("https://localhost:5001/api/users").subscribe(response=>{
+        this.users = response;
+      },error=>{
+        console.log(error)
+      })
+  }
 }
